323b7eb51564c5477ac4f610ab37b95a37fc59e6
Muhammad Nur Alim <muhammadnuralim@students.unnes.ac.id>
2023-03-06 10:16:41 +0700

    adding img result

--- soal5.sql
+++ soal5.sql
@@ -1,8 +1,8 @@
-(SELECT * , count(user_tab.buyerid) OVER(PARTITION BY user_tab.country)
+(SELECT * , count(user_tab.buyerid) OVER(PARTITION BY user_tab.country) as count_buyer_even
 FROM  user_tab, order_tab
 WHERE user_tab.buyerid = order_tab.buyerid  AND MOD(order_tab.itemid,2) = 0)
 UNION ALL
-(SELECT * , MAX(order_tab.gmv) OVER(ORDER BY order_tab.gmv DESC)
+(SELECT * , MAX(order_tab.gmv) OVER(ORDER BY order_tab.gmv DESC) as count_buyer_odd
 FROM  user_tab, order_tab
 WHERE user_tab.buyerid = order_tab.buyerid AND MOD(order_tab.itemid,2) <> 0)
 
